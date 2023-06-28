019cf7c0127e3feb4c4a2bc7069c5a98f8a4a8bd
Denis <danfromav@rambler.ru>
2023-06-28 14:39:00 +0400

    ПР10/4

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -22,18 +22,18 @@ function Main(){
         <main className="content">
         <section className="profile">
           <div className="profile__profile-info">
-            <button type="button" className="profile__avatar-button">
+            <button type="button" className="profile__avatar-button" onClick={handleEditAvatarClick}>
               <img src={avatar} className="profile__avatar" alt="фото" />
             </button>
             <div className="profile__table">
               <div className="profile__info">
                 <h1 className="profile__title">Жак-Ив Кусто</h1>
-                <button type="button" className="profile__edit-button"></button>
+                <button type="button" className="profile__edit-button" onClick={handleEditProfileClick}></button>
               </div>
               <p className="profile__subtitle">Исследователь океана</p>
             </div>
           </div>
-            <button type="button" className="profile__add-button"></button>
+            <button type="button" className="profile__add-button" onClick={handleAddPlaceClick}></button>
         </section>
         <section className="places">
         </section>
